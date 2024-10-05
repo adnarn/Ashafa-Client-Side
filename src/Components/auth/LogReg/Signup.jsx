23bef9934cde25be1f44741a38901bd613519eff
adnarn <adnusy2023@gmail.com>
2024-10-05 16:33:31 +0100

    signup updated link

--- src/Components/auth/LogReg/Signup.jsx
+++ src/Components/auth/LogReg/Signup.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const response = await fetch('http://localhost:4000/user/register', {
+        const response = await fetch('https://ashafa-server.onrender.com/user/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
