a7d1f2e611a3c5d8e5a9435e1e95d642eb9185c3
Dex_Coder <adnusy2023@gmail.com>
2025-01-02 23:52:48 +0100

    Update HomeDash.jsx

--- src/HomeContents/HomeDash.jsx
+++ src/HomeContents/HomeDash.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import styles from './HomeDash.module.css';
-import { Link } from 'react-router-dom';
-import SearchBar from '../Components/SearchBar/SearchBar';
-import { FaPlusCircle } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./HomeDash.module.css";
+import { Link } from "react-router-dom";
+import SearchBar from "../Components/SearchBar/SearchBar";
+import { FaPlusCircle } from "react-icons/fa";
 
 const HomeDash = ({ theme }) => {
-  const role = localStorage.getItem('role');
+  const role = localStorage.getItem("role");
 
   const [items, setItems] = useState([]);
 
@@ -14,22 +14,25 @@ const HomeDash = ({ theme }) => {
     fetchItems(); // Initial fetch of all items
   }, []);
 
-  const fetchItems = (query = '') => {
-    const url = query ? `https://cafe-working-server.vercel.app/search?q=${query}` : 'https://cafe-working-server.vercel.app/';
-    console.log('Fetching items from URL:', url);
-    axios.get(url)
-      .then(result => {
-        console.log('Items fetched successfully:', result.data);
+  const fetchItems = (query = "") => {
+    const url = query
+      ? `https://cafe-working-server.vercel.app/search?q=${query}`
+      : "https://cafe-working-server.vercel.app/";
+    console.log("Fetching items from URL:", url);
+    axios
+      .get(url)
+      .then((result) => {
+        console.log("Items fetched successfully:", result.data);
         setItems(result.data);
       })
-      .catch(err => {
-        console.error('Error fetching items:', err);
+      .catch((err) => {
+        console.error("Error fetching items:", err);
         // Optionally, set an error state to display a message to the user
       });
   };
 
   const handleSearch = (searchQuery) => {
-    console.log('Searching items with query:', searchQuery);
+    console.log("Searching items with query:", searchQuery);
     fetchItems(searchQuery);
   };
 
@@ -53,23 +56,30 @@ const HomeDash = ({ theme }) => {
 
   const groupedItems = groupByDay(items);
 
-
-
   return (
-    <div className={`${styles.mainContent} ${theme === 'light' ? 'light-theme' : 'dark-theme'}`}>
-      <main className={theme === 'light' ? 'light-theme' : 'dark-theme'}>
+    <div
+      className={`${styles.mainContent} ${
+        theme === "light" ? "light-theme" : "dark-theme"
+      }`}
+    >
+      <main className={theme === "light" ? "light-theme" : "dark-theme"}>
         <div className={styles.header}>
           <h2 className={styles.headers}> Dashboard</h2>
-
-          <SearchBar onSearch={handleSearch} /> {/* Add the SearchBar component */}
-
-          <Link to='/add'>
-            <button className={styles.button}><FaPlusCircle className={styles.icons} /> Add Item</button>
+          <SearchBar onSearch={handleSearch} />{" "}
+          {/* Add the SearchBar component */}
+          <Link to="/add">
+            <button className={styles.button}>
+              <FaPlusCircle className={styles.icons} /> Add Item
+            </button>
           </Link>
         </div>
 
         <div className="table-responsive">
-          <table className={`table table-sm ${theme === 'light' ? 'table-light' : 'table-dark'}`}>
+          <table
+            className={`table table-sm ${
+              theme === "light" ? "table-light" : "table-dark"
+            }`}
+          >
             <thead>
               <tr>
                 <th>S/N</th>
@@ -84,40 +94,66 @@ const HomeDash = ({ theme }) => {
                 <React.Fragment key={dateIndex}>
                   {/* Display the date */}
                   <tr>
-                    <td colSpan="5" style={{ fontWeight: 'bold',padding: '10px 0' }}>
+                    <td
+                      colSpan="5"
+                      style={{ fontWeight: "bold", padding: "10px 0" }}
+                    >
                       {date}
                     </td>
                   </tr>
 
                   {/* Display items for that date */}
-                  {groupedItems[date].map((item, index) => (
-                    <tr key={item._id}>
-                      <td>{index + 1}</td>
-                      <td>{item.name}</td>
-                      <td>&#8358;{item.price}</td>
-                      <td>{new Date(item.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true })}</td>
-                      <td>{formatDate(item.date)}</td>
-                    </tr>
-                  ))}
+                  {groupedItems[date].map((entry, entryIndex) =>
+                    entry.items.map((item, itemIndex) => (
+                      <tr key={item._id}>
+                        <td>
+                          {entryIndex + 1}.{itemIndex + 1}
+                        </td>
+                        <td>{item.name}</td>
+                        <td>&#8358;{item.price}</td>
+                        <td>
+                          {new Date(entry.date).toLocaleTimeString([], {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            second: "2-digit",
+                            hour12: true,
+                          })}
+                        </td>
+                        <td>{formatDate(entry.date)}</td>
+                      </tr>
+                    ))
+                  )}
 
                   {/* Display the total price for the day */}
                   <tr>
-                    <td colSpan="2"><strong>Daily Total</strong></td>
+                    <td colSpan="2">
+                      <strong>Daily Total</strong>
+                    </td>
                     <td colSpan="3">
-                      <strong>&#8358;{groupedItems[date].reduce((acc, item) => acc + item.price, 0)}</strong>
+                      <strong>
+                        &#8358;
+                        {groupedItems[date].reduce(
+                          (acc, item) => acc + item.price,
+                          0
+                        )}
+                      </strong>
                     </td>
                   </tr>
                 </React.Fragment>
               ))}
 
-              {  role ==='admin'&&
-             ( <tr>
-                <td colSpan="2"><strong>Overall Total</strong></td>
-                <td colSpan="3">
-                  <strong>&#8358;{items.reduce((acc, item) => acc + item.price, 0)}</strong>
-                </td>
-              </tr>)
-                }
+              {role === "admin" && (
+                <tr>
+                  <td colSpan="2">
+                    <strong>Overall Total</strong>
+                  </td>
+                  <td colSpan="3">
+                    <strong>
+                      &#8358;{items.reduce((acc, item) => acc + item.price, 0)}
+                    </strong>
+                  </td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
