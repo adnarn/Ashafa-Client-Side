da4e817700c2f0d3d1c63e1060abf7b114610122
Dex_Coder <adnusy2023@gmail.com>
2025-01-04 18:50:41 +0100

    Update Reciept.jsx

--- src/PDF/Reciept.jsx
+++ src/PDF/Reciept.jsx
@@ -11,6 +11,7 @@ const Receipt = () => {
   const [customer, setCustomer] = useState('');
   const [approoved, setApprooved] = useState('');
   const [message, setMessage] = useState('');
+  const [refId, setRefId] = useState('');
   const [quantity, setQuantity] = useState([]);
   const [comment, setComment] = useState('');
   const [date, setDate] = useState('');
@@ -19,7 +20,7 @@ const Receipt = () => {
   const [isReceiptVisible, setIsReceiptVisible] = useState(false);
 
   useEffect(() => {
-    axios.get(`https://cafe-working-server.vercel.app/getItem/${id}`)
+    axios.get(`http://localhost:4000/getItem/${id}`)
       .then(result => {
         console.log('Fetched Data:', result.data);
         setName(result.data.items.map(item => item.name));
@@ -27,6 +28,7 @@ const Receipt = () => {
         setQuantity(result.data.items.map(item => item.quantity));
         setDate(result.data.date);
         setCustomer(result.data.customer || 'N/A');
+        setRefId(result.data.referenceId || 'N/A');
         setComment(result.data.comment || '');
         setApprooved('APPROOVED');
         setMessage('Transaction Approoved');
@@ -54,14 +56,20 @@ const Receipt = () => {
           <div ref={receiptRef} className="receipt">
             <div className="receipt-header">
               <img src={logo} alt='' className="company-logo" />
-              <div className="company-name">AshafsLink Business Center</div>
+              <div className="company-name">DosaClick Global Concept</div>
               <div className="company-address">17 College Road Ungwan Dosa, Kaduna</div>
+              <div className="phone">dosaclickglobal@gmail.com</div>
               <div className="phone">09018181999</div>
             </div>
+            {/* <div>{refId}</div> */}
             <div className="receipt-body">
               <table>
                 <tbody>
-                  <tr>
+                <tr>
+                    <td>REF ID</td>
+                    <td>{refId}</td>
+                  </tr>
+                 <tr>
                     <td>CLIENT NAME</td>
                     <td>{customer}</td>
                   </tr>
@@ -78,10 +86,6 @@ const Receipt = () => {
                     <td>{price.reduce((total, itemPrice) => total + itemPrice, 0)}</td>
                   </tr>
                   <tr>
-                    <td>MESSAGE</td>
-                    <td>{message}</td>
-                  </tr>
-                  <tr>
                     <td>COMMENT</td>
                     <td>{comment}</td>
                   </tr>
@@ -96,7 +100,7 @@ const Receipt = () => {
               <div className='approoved'>{approoved}</div>
             </div>
             <div className="receipt-footer">
-              <p>Thanks for working with us</p>
+              <p>For more enquiries, visit: <b>dosaclickventures.com.ng</b><br /> Thank you for your patronization</p>
             </div>
           </div>
           <div style={{ marginTop: '20px', textAlign: 'center' }} >
