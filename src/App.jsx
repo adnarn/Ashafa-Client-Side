4062e7dce9513ecf07da8f5b20fcada5b3a7bb17
Dex_Coder <adnusy2023@gmail.com>
2025-01-06 08:23:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function AppContent() {
     navigate('/login');
   };
 
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("");
 
   const toggle_mode = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
